f4c34365d194cf59339e0e82ea6e59d3d8a95675
Natividad Rivera <riveranatividad1@gmail.com>
2018-11-02 12:39:36 -0700

    Delete twitter_keys.py

--- data-collection/twitter_keys.py
+++
@@ -1,5 +0,0 @@
-#private keys from twitter api
-CONSUMER_KEY ="eucxCfrE3O94mzStUJvAZTi9y"
-CONSUMER_SECRET = "SpgNGGP7vcqFZsZqJhf3eQJ7zLB06nZ8krAQ6fRlXkBQNLGBZh"   
-ACCESS_KEY = "401158605-v4tvRZDpihn403pMmmGBUmWGo2euOFSTvAC7JkrV"    
-ACCESS_SECRET = "JQInFiQvZyaYoEMtVEnkReymbpoHrPcdRUGkzcattNXfj"
